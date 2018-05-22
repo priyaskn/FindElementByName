e0a2a74020f18c72c91a523891ad9eb092767d59
kumaran <kumaran@Priyakumaran>
2018-05-22 13:59:05 -0400

    Pushing the code from local repository to global repository

---
+++ FindElementByName/src/FindElementByName.java
@@ -0,0 +1,18 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FindElementByName {
+
+	public static void main(String args[])
+	
+	{   
+		System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://www.indeed.co.in");
+		driver.findElement(By.name("q")).sendKeys("QTP");
+		driver.findElement(By.name("l")).sendKeys("Calcutta");
+		driver.findElement(By.id("fj")).click();
+		
+	}
+}
